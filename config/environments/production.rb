27a7ed4b11e2062b2c17e2b93171b807a4d89230
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-04-01 23:27:26 +0300

    Revert "[~] Allow all origins temporarly"
    This reverts commit 81a13d65e77e49f44fd778f994399aa27721cc3d.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.allow_origins = '*'
+  config.allow_origins = 'todoapp-ui.herokuapp.com'
 end
